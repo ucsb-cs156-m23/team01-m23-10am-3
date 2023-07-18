0ee4a292a744d30ebbecd571e1ee6b8ee8ed8d1b
Edwin Yee <edwinyee@ucsb.edu>
2023-07-18 01:06:35 -0700

    ey - implement zipcode controller, service, and tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
@@ -43,10 +43,12 @@ public class HomeController {
         resultMap.put("greeting","Greetings from Spring Boot!");
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Andrew Y.");
+        team.add("Mia Z.");
+        team.add("Cindy L.");
+        team.add("Edwin Y.");
+        team.add("Ryan H.");
+
         resultMap.put("team",team);
         resultMap.put("repo","https://github.com/ucsb-cs156-m23/STARTER-team01");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
